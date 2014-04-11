c12f281272971d171dc32966b505b9dec09f1962
julie <jmanahan@sunloft.co.jp>
2014-04-11 14:52:02 +0900

    First committing of the created Ruby project

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = 'af2f69ed8b86ad1025c3a095398ff97324ee0c6753fc247582e39345eeeae7cf244496a9b3c298b199a4e4382492cc9f2caabbe3ab5e82a8fac21787e36152a7'
